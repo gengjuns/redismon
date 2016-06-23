ae84aba5225f32d8e974b187147f37220deec062
gengjun <gengjunlover@gmail.com>
2016-06-23 15:41:55 +0800

    增加cluster node监控

--- bin/redis_cluster.py
+++ bin/redis_cluster.py
@@ -78,6 +78,9 @@ class RedisClusterInfo(object):
         for cluster_node_time in cluster_node_list:
             item_list = cluster_node_time.split(" ")
             self.logger.info(item_list)
+            self.logger.info(len(item_list))
+            self.logger.info(item_list[2].find("fail"))
+
             if (len(item_list) >= 3) and (item_list[2].find("fail") or item_list[2].find("pfail")):
                 cluster_info_dict["cluster_nodes_status"] = 0
                 break
